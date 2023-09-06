6f34ee5a0f9fe874fbe4f148c039c468d7c1607f
Nome Sobrenome <setval42@gmail.com>
2023-09-06 17:26:55 -0300

    first commit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,21 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: "login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"],
 })
 export class LoginComponent implements OnInit {
 
-
-  constructor() {
-
-
+  val = {
+    email: "teste@teste.com",
+    password: "teste12345"
   }
+  
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
 
+  login(loginForm: NgForm, submit: any) {
+    console.log(submit);
+    // console.log(loginForm.value, loginForm.valid, submit);
   }
-
 }
---
+++ src/app/validators/password-strength.validator.ts
@@ -0,0 +1,21 @@
+import { AbstractControl, ValidatorFn, ValidationErrors } from "@angular/forms";
+
+export function createPasswordStrengthValidator(): ValidatorFn {
+
+  return (control:AbstractControl): ValidationErrors | null => {
+      const value = control.value;
+      if(!value) {
+        return null;
+      }
+
+      const hasUpperCase = /[A-Z]+/.test(value);
+
+      const hasLowerCase = /[a-z]+/.test(value);
+
+      const hasNumeric = /[0-9]+/.test(value);
+
+      const passwordValid = hasUpperCase && hasLowerCase && hasNumeric;
+
+      return !passwordValid ? {passwordStrenght: true} : null;
+  }
+}
